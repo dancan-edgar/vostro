8dbfc5baed5a2c2f580bbc13f8e3c286f53bc1b5
Ampeire <ampeireedgar5@gmail.com>
2022-01-19 09:30:04 +0300

    Initial Commit

---
+++ vostro.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package Vostro
+ */
+/*
+ Plugin Name: Vostro
+ PLugin URL: https://github.com/dancan-edgar/methylated-plugin.git
+Description: This is my first Wordpress plugin
+Version: 1.0.0
+Author: Ampeire Edgar
+Author URI: https://github.com/dancan-edgar
+License: GPLv2 or later
+Text Domain: methylated-plugin
+ */
